c33412350041c2ad11eb0175327e7e6e5538fd49
AnnaSeDab <>
2023-07-02 13:48:23 +0000

    add: checkout views and templates

--- fithub/settings.py
+++ fithub/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'merchandise',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fithub.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -72,8 +77,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required for allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
